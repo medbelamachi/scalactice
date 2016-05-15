86a770de3d1d2522de7eaea49912d8879bc5f2fd
Med Belamachi <med.belamachi@gmail.com>
2016-05-15 12:43:25 +0100

    p02 with other approche

--- src/main/scala/P01.scala
+++ src/main/scala/P01.scala
@@ -7,23 +7,33 @@ object P01_28 {
 
   def main(args: Array[String]): Unit = {
     //p02
-    println(lastButOneElement(List(1, 1, 2, 3, 5, 8)))
+    println(lastButOneElement_3(0, List(1, 1, 2, 3, 5, 8)))
     //p01
-    // println(last_01_2(List(1, 1, 2, 3, 5, 8)))
+    // println(last(List(1, 1, 2, 3, 5, 8)))
   }
 
   //p02
+
+  def lastButOneElement_3[A](n: Int, ls: List[A]): A = {
+    if (n <= 0) throw new IndexOutOfBoundsException
+    if (ls.length < n) throw new NoSuchElementException
+    ls.takeRight(n).head
+  }
+
+  /* def lastButOneElement_2(list: List[Int]): Int =
+    if (list.isEmpty) throw new NoSuchElementException else list.init.last
+
   def lastButOneElement(list: List[Int]): Int = list match {
     case a :: b :: Nil => a
     case _ :: tail => lastButOneElement(tail)
     case _ => throw new NoSuchElementException
-  }
+  }*/
 
 
   //p01
-  /*def last_01(list: List[Int]) = list reduceLeft(_ max _)
+  /*def last(list: List[Int]) = list reduceLeft(_ max _)
 
-  def last_01_2(list: List[Int]): Int = list match {
+  def last(list: List[Int]): Int = list match {
     case a :: Nil => a
     case a :: tail => last_01_2(tail)
     case _ => throw new NoSuchElementException
