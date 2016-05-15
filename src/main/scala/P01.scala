249295af6b9f72757ac96a0ada4301c07ec0be2b
Med Belamachi <med.belamachi@gmail.com>
2016-05-15 12:06:44 +0100

    p02

--- src/main/scala/P01.scala
+++ src/main/scala/P01.scala
@@ -6,14 +6,26 @@ object P01_28 {
 
 
   def main(args: Array[String]): Unit = {
-    println(last_01_2(List(1, 1, 2, 3, 5, 8)))
+    //p02
+    println(lastButOneElement(List(1, 1, 2, 3, 5, 8)))
+    //p01
+    // println(last_01_2(List(1, 1, 2, 3, 5, 8)))
   }
 
-  //def last_01(list: List[Int]) = list reduceLeft(_ max _)
+  //p02
+  def lastButOneElement(list: List[Int]): Int = list match {
+    case a :: b :: Nil => a
+    case _ :: tail => lastButOneElement(tail)
+    case _ => throw new NoSuchElementException
+  }
+
+
+  //p01
+  /*def last_01(list: List[Int]) = list reduceLeft(_ max _)
 
   def last_01_2(list: List[Int]): Int = list match {
     case a :: Nil => a
     case a :: tail => last_01_2(tail)
-    case _ => throw new IllegalArgumentException
-  }
+    case _ => throw new NoSuchElementException
+  }*/
 }
