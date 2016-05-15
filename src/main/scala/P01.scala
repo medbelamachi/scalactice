ad09b7361d1ea4bd9d39ca8a68f0e15897c3389a
Med Belamachi <med.belamachi@gmail.com>
2016-05-15 12:01:08 +0100

    p01 find last element using pattern matching

--- src/main/scala/P01.scala
+++ src/main/scala/P01.scala
@@ -6,9 +6,14 @@ object P01_28 {
 
 
   def main(args: Array[String]): Unit = {
-    //p01
-      println( last_01(List(1, 1, 2, 3, 5, 8)))
+    println(last_01_2(List(1, 1, 2, 3, 5, 8)))
   }
 
-  def last_01(list: List[Int]) = list reduceLeft(_ max _)
+  //def last_01(list: List[Int]) = list reduceLeft(_ max _)
+
+  def last_01_2(list: List[Int]): Int = list match {
+    case a :: Nil => a
+    case a :: tail => last_01_2(tail)
+    case _ => throw new IllegalArgumentException
+  }
 }
